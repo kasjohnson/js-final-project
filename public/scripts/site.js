4457d7601fa5f65b44709e8992d1ab408e393a26
Kayla Johnson <kayla.stankowski@gmail.com>
2025-04-24 20:20:34 -0500

    Workday 2- merged files

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -24,6 +24,49 @@ const getEventItemById = async id => {
 const showMenuList = menuItems => {
     menuItems.forEach(({id, name, description, price, url}) => {
         const menuItem = document.createElement('div')
-        menuItem
+        menuItem.className = "menu-item"
+        menuItem.innerHTML = `
+        <img src ="${url}">
+        <h2>${name}</h2>
+        <p> ${description}</p>
+        <p> ${price}</p>
+        `
+        menuItem.onclick = () => showMenuDetails(id)
+        menuList.appendChild(menuItem)
     });
 }
+
+const showMenuDetails = async id => {
+    const {name, description, price, url} = await getMenuItemById(id)
+
+}
+
+;(async () => {
+    const menuItems = await getMenuItems()
+    showMenuList(menuItems)
+})
+
+const showEventList = eventItems => {
+    eventItems.forEach(({id, name, location, date, time}) => {
+        const eventItem = document.createElement('div')
+        eventItem.className = "event-item"
+        eventItem.innerHTML = `
+        <h2>${name}</h2>
+        <p> ${location}</p>
+        <p> ${date}</p>
+        <p> ${time}</p>
+        `
+        eventItem.onclick = () => showEventDetails(id)
+        eventList.appendChild(eventItem)
+    });
+}
+
+const showEventDetails = async id => {
+    const {name, location, date, time} = await getEventItemById(id)
+
+}
+
+;(async () => {
+    const eventItems = await getEventItems()
+    showEventList(eventItems)
+})
